9aed932932cf0fdb7aff9bdd76c1c00589bd0b92
SulthanRaghib <raghib.smp@gmail.com>
2025-05-28 09:19:45 +0700

    move section content file to folder home pages

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,56 +2,56 @@
 @section('content')
     <!-- Hero Section -->
     <section id="hero" class="hero section">
-        @include('home.hero')
+        @include('home.pages.hero')
     </section><!-- /Hero Section -->
 
     <!-- About Section -->
     <section id="about" class="about section">
-        @include('home.about')
+        @include('home.pages.about')
     </section><!-- /About Section -->
 
     <!-- Services Section -->
     <section id="services" class="services section">
-        @include('home.services')
+        @include('home.pages.services')
     </section><!-- /Services Section -->
 
     <!-- Steps Section -->
     <section id="steps" class="steps section">
-        @include('home.steps')
+        @include('home.pages.steps')
     </section><!-- /Steps Section -->
 
     <!-- Call To Action Section -->
     <section id="call-to-action" class="call-to-action section">
-        @include('home.call_to_action')
+        @include('home.pages.call_to_action')
     </section><!-- /Call To Action Section -->
 
     <!-- Testimonials Section -->
     <section id="testimonials" class="testimonials section light-background">
-        @include('home.testimonials')
+        @include('home.pages.testimonials')
     </section><!-- /Testimonials Section -->
 
     <!-- Portfolio Section -->
     <section id="portfolio" class="portfolio section">
-        @include('home.portfolio')
+        @include('home.pages.portfolio')
     </section><!-- /Portfolio Section -->
 
     <!-- Team Section -->
     <section id="team" class="team section light-background">
-        @include('home.team')
+        @include('home.pages.team')
     </section><!-- /Team Section -->
 
     <!-- Pricing Section -->
     <section id="pricing" class="pricing section">
-        @include('home.pricing')
+        @include('home.pages.pricing')
     </section><!-- /Pricing Section -->
 
     <!-- Faq Section -->
     <section class="faq-9 faq section" id="faq">
-        @include('home.faq')
+        @include('home.pages.faq')
     </section><!-- /Faq Section -->
 
     <!-- Contact Section -->
     <section id="contact" class="contact section">
-        @include('home.contact')
+        @include('home.pages.contact')
     </section><!-- /Contact Section -->
 @endsection
