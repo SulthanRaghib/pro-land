8bb63aa0a33a88561f0c120bcb144561e067414c
SulthanRaghib <raghib.smp@gmail.com>
2025-06-30 23:29:07 +0700

    fix: Remove extraneous character from site name in navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,7 +4,7 @@
     <a href="{{ route('home') }}" class="logo d-flex align-items-center me-auto me-xl-0">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="{{ url('assets/img/logo.webp') }}" alt=""> -->
-        <h1 class="sitename">JAS PRO LAND</h1>z
+        <h1 class="sitename">JAS PRO LAND</h1>
     </a>
 
     <nav id="navmenu" class="navmenu">
